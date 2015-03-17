4e49ba833d11a93d18e5d274f532321dbd245ec0
Jason Lyman <jason@lymannuthouse.com>
2015-03-16 18:52:30 -0600

    Fixed link to Bootstrap javascript file

--- inc/footer.php
+++ inc/footer.php
@@ -6,7 +6,7 @@
 
 
 <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
 <script type="text/javascript" src="js/jquery.supercal.js"></script>
 <script type="text/javascript">
